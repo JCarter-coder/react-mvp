83a3996e0348d042acb70b28fac55deb962d5a28
Jesse_Carter <jessetcarter@hotmail.com>
2023-12-17 01:33:54 -0700

    "created some test data"

---
+++ src/testData.js
@@ -0,0 +1,28 @@
+export default {
+    activities: [
+        {
+            id: 111,
+            name: "Watch 10 movies"
+        },
+        {
+            id: 112,
+            name: "Read 20 books"
+        },
+        {
+            id: 113,
+            name: "Take a vacation out of the country"
+        },
+        {
+            id: 114,
+            name: "Learn to play a new instrument"
+        },
+        {
+            id: 115,
+            name: "Do one kind deed a week without attention"
+        },
+        {
+            id: 116,
+            name: "Save $1000"
+        }
+    ]
+}
