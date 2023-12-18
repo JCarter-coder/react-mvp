e4c2bd68de11f9f46f522be26b3b7ac77aadc960
Jesse_Carter <jessetcarter@hotmail.com>
2023-12-18 00:30:55 -0700

    "corrected objects in seed file"

--- database/seed.sql
+++ database/seed.sql
@@ -1,9 +1,9 @@
-INSERT INTO activities_table (activity) VALUES ({name:"Watch 10 movies"});
-INSERT INTO activities_table (activity) VALUES ({name:"Read 20 movies"});
-INSERT INTO activities_table (activity) VALUES ({name:"Take a vacation out of the country"});
-INSERT INTO activities_table (activity) VALUES ({name:"Learn to play a new instrument"});
-INSERT INTO activities_table (activity) VALUES ({name:"Do one kind deed a week without recognition"});
-INSERT INTO activities_table (activity) VALUES ({name:"Save $1000"});
-INSERT INTO activities_table (activity) VALUES ({name:"Save $2000"});
-INSERT INTO activities_table (activity) VALUES ({name:"Save $1,000,000!"});
-INSERT INTO activities_table (activity) VALUES ({name:"Become master of all space, time, and dimension"});
+INSERT INTO activities_table (activity) VALUES ('{"name": "Watch 10 movies"}');
+INSERT INTO activities_table (activity) VALUES ('{"name": "Read 20 movies"}');
+INSERT INTO activities_table (activity) VALUES ('{"name": "Take a vacation out of the country"}');
+INSERT INTO activities_table (activity) VALUES ('{"name": "Learn to play a new instrument"}');
+INSERT INTO activities_table (activity) VALUES ('{"name": "Do one kind deed a week without recognition"}');
+INSERT INTO activities_table (activity) VALUES ('{"name": "Save $1000"}');
+INSERT INTO activities_table (activity) VALUES ('{"name": "Save $2000"}');
+INSERT INTO activities_table (activity) VALUES ('{"name": "Save $1,000,000!"}');
+INSERT INTO activities_table (activity) VALUES ('{"name": "Become master of all space, time, and dimension"}');
