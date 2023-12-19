5a1f474c6e1e54203d6fab6db5afafd134f5a76d
Jesse_Carter <jessetcarter@hotmail.com>
2023-12-19 09:33:27 -0700

    "changed URL"

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 
 const PORT = 8000;
-const URL = '/api';
+const URL = '/api/resolutions';
 const { Pool } = pg;
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL
