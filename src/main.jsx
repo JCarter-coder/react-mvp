5c5d65eda438a86dc546d623cdea16e41b3db5c6
Jesse_Carter <jessetcarter@hotmail.com>
2023-12-24 23:37:50 -0700

    added color palette

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
 import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 //import './index.css'
 
 //TODO: adjust color palette 
+const colors = {
+  primary: '#42BFDD', //aeroBlue
+  secondary: '#BBE6E4', //mintGreen
+  white: '#F0F6F6', //antiFlashWhite
+  highlight: '#254E70', // indigoDye
+  dark: '#493B2A' // taupe
+}
+
+const theme = extendTheme({ colors });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
