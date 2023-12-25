de7f4d06bd5a940c2483c45d8ab4c8d0df14b32d
Jesse_Carter <jessetcarter@hotmail.com>
2023-12-24 23:17:19 -0700

    working to insert color palette

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 //import './index.css'
 
+//TODO: adjust color palette 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
